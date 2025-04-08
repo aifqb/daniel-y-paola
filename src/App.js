c3316012c40605daea8c4ee94935c1ca0db11e88
alex francesc <76713839+aifqb@users.noreply.github.com>
2025-04-08 17:26:59 -0400

    primera subida

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Puedes crear este archivo para estilos
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Invitación de Boda</h1>
+        <p>Daniel & Paola</p>
+        {/* ... añade aquí el resto de la información de tu boda ... */}
+        <div className="ubicaciones">
+          <h2>Ubicaciones:</h2>
+          <p><strong>Ceremonia Religiosa:</strong> Iglesia de "San Pedro", plaza Sucre</p>
+          <p><strong>Recepción:</strong> Salón de Eventos "La orquídea", Ciudad Satélite.</p>
+        </div>
+        {/* ... más información ... */}
+        <p>¡Los esperamos!</p>
       </header>
     </div>
   );
